6302966697fcb49448b5c748f9c39301d1d5b044
Vishal Mistry <Vishal.Mistry@act.local>
2026-03-31 09:53:09 +0530

    Abstract All Page

--- books/spiders/quotes_spider.py
+++ books/spiders/quotes_spider.py
@@ -4,7 +4,6 @@ from scrapy.cmdline import execute
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-    allowed_domains = ["quotes.toscrape.com"]
     start_urls = ["http://quotes.toscrape.com/"]
 
     def parse(self, response):
@@ -14,6 +13,10 @@ class QuotesSpider(scrapy.Spider):
             item["author"] = quote.css("small.author::text").get()
             item["tags"] = quote.css("div.tags a.tag::text").getall()
             yield item
+
+        next_page = response.css("li.next a::attr(href)").get()
+        if next_page is not None:
+            yield response.follow(next_page,callback=self.parse)
 
 if __name__ == '__main__':
     execute("scrapy crawl quotes".split())
